49e9e426e43d50cdc93874e1bb13a8562fee0f66
kevin <kevin@PFSNSQ858384.enterprise.internal.city.ac.uk>
2018-04-17 18:22:56 +0100

    We basically changd the code so that the force field only applies to the guid layer. The mac layer currently follows the guid one, and we will add the same funcitonality with the id. That requires to then change size of circles based on children, and position them in a way that avoids overlay (e.g. position + rotation offset depending on number of children).

--- src/network.js
+++ src/network.js
@@ -33,12 +33,11 @@ export default function chart(id) {
       _background = display[theme].background;
     }
 
-    
     selection.each(function() {
       let node = select(this);  
       let data = node.datum() || [];
       //let sh = height || Math.round(width * DEFAULT_ASPECT);4
-      let shW = 800;
+      let sw = 800;
       let sh = 600 ;
       
       // SVG element
@@ -46,7 +45,7 @@ export default function chart(id) {
       if (id) sid = 'svg-' + id;
 
       let root = svg(sid)
-                  .width(shW).height(sh).margin(margin).scale(scale)
+                  .width(sw).height(sh).margin(margin).scale(scale)
                   .background(_background);
       let tnode = node;
       if (transition === true) {
@@ -60,7 +59,6 @@ export default function chart(id) {
         // build a style sheet from the embedded charts
         _style = [ _impl ].filter(c => c != null).reduce((p, c) => p + c.defaultStyle(theme, w), '');
       } 
-
 
       root.style(_style);
       tnode.call(root);
@@ -79,39 +77,102 @@ export default function chart(id) {
       var simulation = d3.forceSimulation()
         .force("link", d3.forceLink().id(function(d) { return d.id; }))
         .force("charge", d3.forceManyBody())
-        .force("center", d3.forceCenter(shW / 2, sh / 2));
+        .force("center", d3.forceCenter(sw / 2, sh / 2))
+        ;
       
       let nodeEnter = d3.select(".chart-network");
       let nodes = [], 
+          mac=[],
+          ip=[],
+          relationshipsGuidToMac=[],
+          relationshipsMacToIp=[],
           links = []; 
-      nodes = data.nodes;
-      links = data.links;
+
+      nodes = data.nodes, mac=data.mac, ip=data.ip, relationshipsGuidToMac = data.relationshipsGuidToMac,
+      relationshipsMacToIp=data.relationshipsMacToIp,links = data.links;
 
       console.log("nodeEnter: "); console.log(nodeEnter);
       console.log("nodes: "); console.log(nodes);
+      console.log("mac: "); console.log(mac);
+      console.log("ip: "); console.log(ip);
+      console.log("relationshipsMacToIp: "); console.log(relationshipsMacToIp);
       console.log("links: "); console.log(links);
 
+      // test
+      var ips = tryGetChildren("mac_32_64_23_34");
+      console.log("ips: "); console.log(ips);
+
+      // guid circle
       let nodeCircle = nodeEnter.append("g")
         .attr("class", "nodes")
         .selectAll("circle")
         .data(nodes)
         .enter().append("circle")
-          // .attr("r", 5)
-          .attr("fill",  0 )
-          .attr("cx", function(d,i) { console.log(d); console.log(i); return w*Math.random(); })
           .attr("id", d => d.id )
           .attr("class", function(d) { 
             if (d.id.includes("ip")) { return "ipCircle" } 
             else if (d.id.includes("mac")) {return "macCircle" }
             return "guidCircle"; 
           })
-          .attr("r", function(d){ if (d.id.includes("guid")) {return 10} 
+          .attr("r", function(d){ if (d.id.includes("guid")) {return 20} 
             else if (d.id.includes("mac")) {return 5} 
             return 2; })
+          .on("click", function(d){
+            console.log("clicked on d: "); console.log(d);
+            console.log("tryGetChildren: "); console.log(tryGetChildren(d.id));
+          })
           .call(d3.drag()
             .on("start", dragstarted)
             .on("drag", dragged)
             .on("end", dragended));
+        
+      // mac circle
+      let macCircle = nodeEnter.append("g")
+      .attr("class", "nodes")
+      .selectAll("circle")
+      .data(mac)
+      .enter().append("circle")
+        .attr("fill",  0 )
+        .attr("id", d => d.id )
+        .attr("class", function(d) { 
+          if (d.id.includes("ip")) { return "ipCircle" } 
+          else if (d.id.includes("mac")) {return "macCircle" }
+          return "guidCircle"; 
+        })
+        .attr("r", function(d){ if (d.id.includes("guid")) {return 10} 
+          else if (d.id.includes("mac")) {return 5} 
+          return 2; })
+        .on("click", function(d){
+          console.log("clicked on d: "); console.log(d);
+          console.log("tryGetChildren: "); console.log(tryGetChildren(d.id));
+        })
+        .on("tick", function(d){
+          console.log("tick from mac");
+        })
+      ;
+
+      // ip circle
+      let ipCircle = nodeEnter.append("g")
+      .attr("class", "nodes")
+      .selectAll("circle")
+      .data(ip)
+      .enter().append("circle")
+        .attr("fill",  0 )
+        .attr("id", d => d.id )
+        .attr("class", function(d) { 
+          if (d.id.includes("ip")) { return "ipCircle" } 
+          else if (d.id.includes("mac")) {return "macCircle" }
+          return "guidCircle"; 
+        })
+        .attr("r", function(d){ if (d.id.includes("guid")) {return 10} 
+          else if (d.id.includes("mac")) {return 5} 
+          return 2; })
+        .on("click", function(d){
+          console.log("clicked on d.id: "); console.log(d.id);
+          console.log("tryGetParent circle: "); 
+          console.log(tryGetParent(d.id));
+        })
+        ;
 
       let textNode = nodeEnter
        .append("g")
@@ -132,12 +193,41 @@ export default function chart(id) {
           d3.select(this)._groups[0][0].style.fill = "black";
         })
         .on("mouseout", function (d){
-          console.log("mouseout, this is: ");console.log(this );
-          console.log("mouseout, d3.select(this) is: ");console.log(d3.select(this) );
+          // console.log("mouseout, this is: ");console.log(this );
+          // console.log("mouseout, d3.select(this) is: ");console.log(d3.select(this) );
           d3.select(this)._groups[0][0].style.fill = "";
         })
-        
+        .call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended))    
         ;
+
+        let textMac = nodeEnter
+        .append("g")
+         .selectAll("text")
+         .data(mac)
+         .enter().append("text")
+         .text(function(d) { return d.id; })
+         .attr("id", d => d.id)
+         .attr("class", "textNetwork")
+         .attr("class", function(d) { 
+           if (d.id.includes("ip")) { return "ipText" } 
+           else if (d.id.includes("mac")) {return "macText" }
+           return "guidText";
+         })
+         .on("mouseover", function (d){
+           d3.select(this)._groups[0][0].style.fill = "black";
+         })
+         .on("mouseout", function (d){
+           d3.select(this)._groups[0][0].style.fill = "";
+         })
+         .call(d3.drag()
+         .on("start", dragstarted)
+         .on("drag", dragged)
+         .on("end", dragended))    
+         ;
+ 
 
       var link = nodeEnter.append("g")
           .attr("class", "links")
@@ -149,7 +239,8 @@ export default function chart(id) {
 
       simulation
         .nodes(data.nodes)
-        .on("tick", ticked);
+        .on("tick", ticked)        
+        ;        
     
       simulation.force("link")
         .links(data.links);    
@@ -162,24 +253,45 @@ export default function chart(id) {
           .attr("y2", function(d) { return d.target.y; });
 
         nodeCircle
-          .attr("cx", function(d) { return d.x; })
-          .attr("cy", function(d) { return d.y; });
+          .attr("cx", function(d) { 
+            return d.x = Math.max(this.r.animVal.value, Math.min(sw - this.r.animVal.value, d.x)); })
+          .attr("cy", function(d) { return d.y = 
+            Math.max(this.r.animVal.value, Math.min(sh - this.r.animVal.value, d.y)); })        
+
+        macCircle
+          .attr("cx", function(d){
+            var idParent = tryGetParent(d.id);
+            // console.log("macCircle idParent: "+idParent);
+            return d3.select("#"+idParent)._groups[0][0].cx.animVal.value;
+          })
+          .attr("cy", function(d){
+            var idParent = tryGetParent(d.id);
+            // console.log("macCircle idParent: "+idParent);
+            return d3.select("#"+idParent)._groups[0][0].cy.animVal.value;
+          })
 
         textNode
           .attr("x", function(d) { 
             return d3.select("#"+d.id)._groups[0][0].cx.animVal.value ; } )
-          .attr("y", function(d) { return d3.select("#"+d.id)._groups[0][0].cy.animVal.value ; } )        
-      }
+          .attr("y", function(d) { return d3.select("#"+d.id)._groups[0][0].cy.animVal.value ; } )
+      
+          textMac
+          .attr("x", function(d) { 
+            return d3.select("#"+d.id)._groups[0][0].cx.animVal.value ; } )
+          .attr("y", function(d) { return d3.select("#"+d.id)._groups[0][0].cy.animVal.value ; } )
+          
+        }
+
 
       function dragstarted(d) {
         if (!d3.event.active) simulation.alphaTarget(0.3).restart();
         d.fx = d.x;
-        d.fy = d.y;
+        d.fy = d.y;        
       }
       
       function dragged(d) {
-        d.fx = d3.event.x;
-        d.fy = d3.event.y;
+        d.fx = Math.max(10, Math.min(sw -10, d3.event.x));
+        d.fy = Math.max(10, Math.min(sh-10, d3.event.y));
       }
       
       function dragended(d) {
@@ -187,11 +299,11 @@ export default function chart(id) {
         d.fx = null;
         d.fy = null;
       }
-            
+    }); 
+    
+  
 
 
-
-    });    
   }
   
   _impl.self = function() { return 'g' + (id ?  '#' + id : '.' + classed); }
@@ -201,46 +313,37 @@ export default function chart(id) {
   };
 
   _impl.defaultStyle = () => `
-                  ${_impl.self()} { 
-                    shape-rendering: geometricprecision;
-                  }
-                `;
+      ${_impl.self()} { 
+        shape-rendering: geometricprecision;
+      }
+    `;
   
   _impl.classed = function(value) {
     return arguments.length ? (classed = value, _impl) : classed;
-  };
-    
+  };    
   _impl.background = function(value) {
     return arguments.length ? (background = value, _impl) : background;
   };
-
   _impl.theme = function(value) {
     return arguments.length ? (theme = value, _impl) : theme;
-  };  
-
+  };
   _impl.size = function(value) {
     return arguments.length ? (width = value, height = null, _impl) : width;
   };
-    
   _impl.width = function(value) {
     return arguments.length ? (width = value, _impl) : width;
-  };  
-
+  };
   _impl.height = function(value) {
     return arguments.length ? (height = value, _impl) : height;
   }; 
-
   _impl.scale = function(value) {
     return arguments.length ? (scale = value, _impl) : scale;
   }; 
-
   _impl.margin = function(value) {
     return arguments.length ? (margin = value, _impl) : margin;
   };   
-
   _impl.style = function(value) {
     return arguments.length ? (style = value, _impl) : style;
   }; 
-
   return _impl;
 }
